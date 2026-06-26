f93e4472d0af58dc06619260ca6a7e54bb4274eb
Bala Kumaravel <bala@jacard.ai>
2026-06-26 11:55:52 +0530

    legacy

---
+++ legacy/contact.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>Arjith Property Developers | Contact</title>
+  <link rel="stylesheet" type="text/css" href="css/apd_style.css" />
+  <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+  <script type="text/javascript" src="js/jquery.validate.min.1.9.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function(){
+    $("#contact").validate();
+  });
+  </script>
+</head>
+
+<body>
+<!-- Begin Header Wrapper -->
+<div id="headerwrapper">
+  <div id="hw_inner">
+    <div id="header">
+      <div class="logo"><a href="index.html"><img src="images/logo.png" alt="Arjith Property Developers" title="Arjith Property Developers" /></a></div>
+        <div class="header_right">
+        <div class="hr_top">
+          <ul class="hr_navi_top">
+            <li><a href=""><img src="images/home.png" alt="" title="" /></a></li>
+            <li><a href=""><img src="images/sitemap.png" alt="" title="" /></a></li>
+            <li><a href=""><img src="images/mail.png" alt="" title="" /></a></li>
+          </ul>
+        </div>
+        <div class="hr_bottom">
+         <div class="helpline"><img src="images/helpline.png" alt="Arjith Property Developers" title="Arjith Property Developers" /></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- End Wrapper Header -->   
+<!-- Begin Container Wrapper -->
+<div id="inner_container_wrapper">
+  <div id="container-inner">
+    <div id="containerbody">
+     <div id="inner_navigation">
+        <ul>
+         <li><a href="index.html">HOME</a></li>
+         <li><a href="aboutus.html"> ABOUT US</a></li>
+         <li><a href="project.html">PROJECTS</a></li> 
+         <li><a href="enquiry.php">ENQUIRY</a></li>
+         <li><a class="current"  href="contact.php">CONTACT US</a></li>
+         </ul>
+         </div>
+     <div id="container">
+     <!-- Begin Left Column -->
+     <div id="leftcolumn">
+      <div class="left_title"><h1>Arjith Property Developers Enquiry Form</h1></div>
+        <div class="res_comm_pro">
+          <div class="sn_enquiry_area">
+            <p>
+                        Please fillup the following form for Business Contact.<br />
+            * Fields are mandatory </p>
+            <form action="contact.php" method="post" id="contact">     
+            <input type="hidden" name="do" value="sendContact">              
+            <table>
+              <tr><td class="estyle1">Name : </td><td><input class="estyle2 required" type="text" size="22" name="name"></td></tr>
+              <tr><td style="height:10px;"></td><td></td></tr>
+              <tr><td class="estyle1">E-Mail: </td><td><input class="estyle2 required email"  type="text" size="22" name="email"></td></tr>
+              <tr><td style="height:10px;"></td><td></td></tr>
+              <tr><td class="estyle1">Phone :</td><td><input class="estyle2 required" type="text" size="22" name="phone"></td></tr>
+              <tr><td style="height:10px;"></td><td></td></tr>
+              <tr><td class="estyle1">Message :</td><td><textarea class="estyle3 required" cols="37" name="message" rows="6"></textarea></td></tr>
+              <tr><td style="height:10px;"></td><td></td></tr>
+              <tr><td></td><td><input type="submit" value="Submit"> <input type="reset" value="Reset" id="Submit" > </td></tr>
+            </table>
+            </form>
+          </div>
+        </div>
+      </div>
+  <!-- End Left Column -->
+  <!-- Begin Right Column -->
+		  <div id="rightcolumn">
+        <div class="right_title"><h1>Reach Us</h1></div>
+        <div class="right_p">
+          Engineer & Promoter : V.Gouthaman <span>B.E.,M.I.E.,F.I.V.,</span><br />
+          Trichy-620017,Mobile - 90471 86510<br /><br />
+          Arjith Villa,<br />
+          AA-32, Arjith Property Developers, 
+Ukkirakali Amman Kovil Street, Tennur, 
+Trichy 620017<br />
+          Mobile : 90471 86510<br />
+          Email : <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="0d6c7f676479657d7f627d687f79744d6a606c6461236e6260">[email&#160;protected]</a>
+        </div>
+        <!--<div class="right_title"><h1>Testimonials</h1></div>
+        <div class="testmonial_p">Ashiana means a family, with peace of mind and fresh air with smile on face. <br /><span>- Raaniraj , Resident, Arjith Aristocrat.</span></div>-->
+      </div>
+    </div>
+   </div>
+  </div>
+</div>
+<!-- End Right Column -->
+<!-- End Container Wrapper -->
+<!-- Begin Footer Wrapper-->
+<div id="footer-wrapper">
+         <div id="footer-inner">
+         <div id="footer">
+         <div id="footerbox">
+         <h1>MAIN CONTENT</h1>
+         <ul class="fnavi">
+         <li><a href="index.html">Home</a></li>
+         <li><a href="aboutus.html">About Us</a></li>
+         <li><a href="enquiry.php">Make Your Enquiry</a></li>
+         <li><a href="project.html">Complete / Current Projects</a></li>
+         <li><a href="">Our Services</a></li>
+         <li><a href="contact.php">Contact Us</a></li>
+         <li><a href="">Site Map</a></li>
+         </ul>
+         </div>
+         <div id="footerbox">
+         <h1>OUR TEAM</h1>
+<div class="f_content_1"><strong>Engineer & Promoter</strong><br />V.Gouthaman B.E.,M.I.E.,F.I.V.,<br />Trichy-18,98424-76510</div>
+<div class="f_content_1"><strong>ARCHITECT</strong><br /><!--S.S.Associates,<br />Trichy-21-->S.Subramanian A.I.I.A.,<br />S.S.Associates,Trichy-21.</div>
+         </div>
+         <div id="footerbox">
+         <h1>SMS SUPPORT</h1>
+         <div class="f_content_1">For any queries, type and send SMS</div>
+         </div>
+         <div id="footerbox">
+         <h1>CONTACT US</h1>
+         <div class="f_content_1">Arjith Villa,<br />AA-32, Arjith Property Developers,<br />
+Ukkirakali Amman Kovil Street, Tennur, 
+Trichy 620017 <br />
+Tamilnadu.</div>
+<div class="f_content_1">
+
+Mo: +91 90471 86510<br />
+
+Em: <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="96f7e4fcffe2fee6e4f9e6f3e4e2efd6f1fbf7fffab8f5f9fb">[email&#160;protected]</a></div>
+         </div>
+         <div class="footer_copy">&copy; 2013 Ajith Property Developers. All Rights Reserved &nbsp; | &nbsp; <a href="">Privacy Policy </a> &nbsp; | &nbsp; <a href="">Sitemap</a></div>
+         </div>
+         </div>
+         </div>
+<!-- End Footer Wrapper-->      
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'a11a1e1c5f057f8f',t:'MTc4MjQ1NDk0Nw=='};var a=document.createElement('script');a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
+</html>
